289d588975c111f03093019820b6d03c611954a2
Rechkinn <al.red197218@gmail.com>
2025-09-24 20:06:41 +0700

    тест scss, компонента Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,44 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1>Это будет второй коммит</h1>
-        <h2>Это будет второй коммит</h2>
-        <h3>Это будет второй коммит</h3>
-        <h4>Это будет второй коммит</h4>
-        <h5>Это будет второй коммит</h5>
-        <h6>Это будет второй коммит</h6>
-      </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <Header />
+      <>
+        <div>
+          <h1>Это будет второй коммит</h1>
+          <h2>Это будет второй коммит</h2>
+          <h3>Это будет второй коммит</h3>
+          <h4>Это будет второй коммит</h4>
+          <h5>Это будет второй коммит</h5>
+          <h6>Это будет второй коммит</h6>
+        </div>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </>
     </>
   );
 }
