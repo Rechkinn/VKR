8c18bf287c71997855be5c6b33ec7bd28234d787
Rechkinn <al.red197218@gmail.com>
2025-10-04 18:57:13 +0700

    Добавил компонент Profile

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,11 @@
+import ProfileInfo from "../profile-info/profile-info";
+import styles from "./profile.module.css";
+
+export default function Profile() {
+  return (
+    <section>
+      <ProfileInfo />
+      {/* Компонент для отображения автомобилей */}
+    </section>
+  );
+}
