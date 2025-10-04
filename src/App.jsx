791c954becd4de2deebb7cc4fed6ec8b4c768223
Rechkinn <al.red197218@gmail.com>
2025-10-04 18:56:37 +0700

    Добавил компонент Profile со статичными данными (пока что)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Header from './components/Header'
+import React from "react";
+import Header from "./components/Header";
+import Profile from "./components/profile/profile";
 
 export default function App() {
   return (
     <div>
       <Header />
+
       <main style={{ padding: 16 }}>
-        <p>This template mocks Telegram.WebApp in the browser so you can use React DevTools.</p>
-        <p>Open the console and try: <code>window.Telegram && window.Telegram.dispatch('ready')</code></p>
+        {/* <p>This template mocks Telegram.WebApp in the browser so you can use React DevTools.</p>
+        <p>Open the console and try: <code>window.Telegram && window.Telegram.dispatch('ready')</code></p> */}
+        <Profile />
       </main>
     </div>
-  )
+  );
 }
