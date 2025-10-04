959e87119dae91deec7dc36bc7dd81751b76e1e9
Rechkinn <al.red197218@gmail.com>
2025-10-04 18:57:34 +0700

    Добавил компонент ProfileInfo

---
+++ src/components/profile-info/profile-info.jsx
@@ -0,0 +1,30 @@
+import styles from "./profile-info.module.css";
+import star from "../../image/star.svg";
+import edit from "../../image/edit-profile.svg";
+import Button from "../button/button";
+
+const url =
+  "https://sun9-81.userapi.com/s/v1/if2/U3hQGq7ILnj0qNSHQHWk-cGAi0CC_aptfRfpvCoyiauKMl7wMMxqB-oGrELsiuMnJ46UPd-sIlDPv5lANtLm-XlQ.jpg?quality=95&as=32x20,48x30,72x45,108x67,160x100,240x149,360x224,480x299,540x336,640x398,720x448,1080x672,1157x720&from=bu&cs=1157x0";
+
+export default function ProfileInfo() {
+  return (
+    <article className={`global-styles ${styles.container}`}>
+      <img src={url} alt="" className={styles.mainImage} />
+      <h1 className={styles.name}>Ишмамбекдулла Фахрутдиновских</h1>
+      <div className={styles.containerRoleAndRating}>
+        <div className={styles.role}>Водитель</div>
+        <img src={star} alt="Звезда" className={styles.star} />
+        <span className={styles.rating}>4.9</span>
+      </div>
+      <p className={styles.startedWorking}>Работаю с Января 2025</p>
+      <Button styleType="black" isButtonWithIcon={true}>
+        <img
+          src={edit}
+          alt="Редактировать профиль"
+          className={styles.iconButton}
+        />
+        Редактировать профиль
+      </Button>
+    </article>
+  );
+}
