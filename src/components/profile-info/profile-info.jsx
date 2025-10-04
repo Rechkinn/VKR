86fd76406a41459834d04f1d1c8d59d9d343c7d0
Rechkinn <al.red197218@gmail.com>
2025-10-04 22:13:59 +0700

    5.14

--- src/components/profile-info/profile-info.jsx
+++ src/components/profile-info/profile-info.jsx
@@ -8,9 +8,10 @@ import Button from "../button/button";
 //   "https://sun9-81.userapi.com/s/v1/if2/U3hQGq7ILnj0qNSHQHWk-cGAi0CC_aptfRfpvCoyiauKMl7wMMxqB-oGrELsiuMnJ46UPd-sIlDPv5lANtLm-XlQ.jpg?quality=95&as=32x20,48x30,72x45,108x67,160x100,240x149,360x224,480x299,540x336,640x398,720x448,1080x672,1157x720&from=bu&cs=1157x0";
 
 export default function ProfileInfo({ userInfo }) {
-  function parseData(string) {
-    const obj = JSON.parse(string);
-    return typeof obj.initDataUnsafe;
+  function parseData() {
+    // const obj = JSON.parse(userInfo);
+    // return typeof obj.initDataUnsafe;
+    return typeof userInfo;
     // .user.first_name;
   }
 
@@ -34,7 +35,7 @@ export default function ProfileInfo({ userInfo }) {
       <div>
         -------------------------------------------------------------------------------------------------------------------------------------------
       </div>
-      {parseData(userInfo)}
+      {parseData()}
       <div>
         -------------------------------------------------------------------------------------------------------------------------------------------
       </div>
