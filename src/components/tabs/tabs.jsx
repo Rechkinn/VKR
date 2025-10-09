c2da26ad72fbf84d9f17441e4c2be63caf397cfc
Rechkinn <al.red197218@gmail.com>
2025-10-09 20:28:41 +0700

    Создан Компоненты Tabs

---
+++ src/components/tabs/tabs.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import styles from "./tabs.module.css";
+import Button from "../button/button";
+
+export default function Tabs() {
+  const [currentTab, setCurrentTab] = useState("Active");
+
+  return (
+    <div className={styles.tabs}>
+      <Button
+        className={
+          currentTab === "Active"
+            ? `${styles.button} ${styles.activeTab}`
+            : `${styles.button}`
+        }
+        onClick={() => setCurrentTab("Active")}
+      >
+        Активные
+      </Button>
+
+      <div
+        className={
+          currentTab === "Completed"
+            ? `${styles.separator}`
+            : `${styles.separator} ${styles.separatorHidden}`
+        }
+      ></div>
+
+      <Button
+        className={
+          currentTab === "Upcoming"
+            ? `${styles.button} ${styles.activeTab}`
+            : `${styles.button}`
+        }
+        onClick={() => setCurrentTab("Upcoming")}
+      >
+        Предстоящие
+      </Button>
+
+      <div
+        className={
+          currentTab === "Active"
+            ? `${styles.separator}`
+            : `${styles.separator} ${styles.separatorHidden}`
+        }
+      ></div>
+
+      <Button
+        className={
+          currentTab === "Completed"
+            ? `${styles.button} ${styles.activeTab}`
+            : `${styles.button}`
+        }
+        onClick={() => setCurrentTab("Completed")}
+      >
+        Завершенные
+      </Button>
+    </div>
+  );
+}
