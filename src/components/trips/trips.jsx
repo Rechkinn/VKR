69c03ea26148f94e14141e6f31fe46a5c910d52a
Rechkinn <al.red197218@gmail.com>
2025-10-09 20:29:07 +0700

    Создан компонент trips

---
+++ src/components/trips/trips.jsx
@@ -0,0 +1,35 @@
+import Balance from "../balance/balance";
+import Button from "../button/button";
+import styles from "./trips.module.css";
+import carIcon from "../../image/navbar/carActive.svg";
+import Tabs from "../tabs/tabs";
+
+export default function Trips() {
+  return (
+    <section>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Мои поездки</h1>
+        <div className={styles.balance}>
+          <Balance balanceValue={2.743} />
+          <Button className="black">Пополнить</Button>
+        </div>
+      </header>
+      <div className={styles.containerCreateTrip}>
+        {true ? (
+          <Button className={styles.buttonCreateTrip}>
+            <span className={styles.buttonIconBackground}>
+              <img src={carIcon} alt="Иконка автомобиля" />
+            </span>
+            <span className={styles.buttonText}>Создать поездку</span>
+          </Button>
+        ) : (
+          <form></form>
+        )}
+      </div>
+
+      <Tabs />
+
+      <div></div>
+    </section>
+  );
+}
