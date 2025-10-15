a3d1fab0d740d7727620a41422e92de4365e76bd
Aleksey Redkin <al.red197218@gmail.com>
2025-10-16 02:05:55 +0700

    Создание экшена и редьюсера для информации о пользователе

---
+++ src/services/reducers/user.js
@@ -0,0 +1,32 @@
+import { SET_USER_TELEGRAM_INFO } from "../actions/user";
+
+// const initialState = {
+//   infoFromTelegram: {
+//     id: 870593529,
+//     first_name: "Aleks",
+//     last_name: "",
+//     username: "Rechkinnnn",
+//     language_code: "ru",
+//     allows_write_to_pm: true,
+//     photo_url:
+//       "https://t.me/i/userpic/320/JtGPbJBctx4LLq_nm2R9calFGN2KxnnIKkyKLZQyodE.svg",
+//   },
+//   infoFromBackend: null,
+// };
+
+const initialState = {
+  infoFromTelegram: null,
+  infoFromBackend: null,
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER_TELEGRAM_INFO:
+      return {
+        ...state,
+        infoFromTelegram: action.infoFromTelegram,
+      };
+    default:
+      return state;
+  }
+};
