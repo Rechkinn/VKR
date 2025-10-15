27d8c4ac5d40ae1a5d5d412641e5563d39eafac4
Aleksey Redkin <al.red197218@gmail.com>
2025-10-16 02:08:50 +0700

    рефактор кода

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,13 +1,10 @@
-import { useState, useEffect } from "react";
 import Cars from "../cars/cars";
 import ProfileInfo from "../profile-info/profile-info";
 import styles from "./profile.module.css";
 import ChangeProfileInfo from "../change-profile-info/change-profile-info";
 import { useSelector } from "react-redux";
 
-export default function Profile({ userInfo }) {
-  // const [userData, setUserData] = useState(null);
-
+export default function Profile() {
   const { activeSection } = useSelector((store) => store.profile);
 
   // useEffect(() => {
@@ -29,43 +26,17 @@ export default function Profile({ userInfo }) {
   // setUserData(parsed);
   // }, []);
 
-  // function parsingUserInfo() {
-  //   if (!userInfo) return <div style={{ color: "#666" }}>No user info</div>;
-
-  //   let parsed;
-  //   try {
-  //     parsed = typeof userInfo === "string" ? JSON.parse(userInfo) : userInfo;
-  //   } catch (err) {
-  //     return (
-  //       <div style={{ color: "#d32f2f" }}>
-  //         <div>Invalid JSON: {err.message}</div>
-  //         <pre style={{ whiteSpace: "pre-wrap" }}>{String(userInfo)}</pre>
-  //       </div>
-  //     );
-  //   }
-
-  //   if (!parsed || typeof parsed !== "object") {
-  //     return <div>{String(parsed)}</div>;
-  //   }
-  // }
-
   return (
     <>
-      {userInfo && (
+      {activeSection === "info" && (
         <>
-          {activeSection === "info" && (
-            <>
-              {/* <ProfileInfo userData={userData} /> */}
-              <ProfileInfo userData={userInfo} />
-              {/* Здесь будет компонент для отображения автомобилей */}
-              {/* <Cars /> */}
-            </>
-          )}
-          {activeSection === "changeInfo" && (
-            <ChangeProfileInfo userData={userInfo} />
-          )}
+          {/* <ProfileInfo userData={userData} /> */}
+          <ProfileInfo />
+          {/* Здесь будет компонент для отображения автомобилей */}
+          {/* <Cars /> */}
         </>
       )}
+      {activeSection === "changeInfo" && <ChangeProfileInfo />}
     </>
   );
 }
