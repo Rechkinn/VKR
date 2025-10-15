112a3a27b44b72127981ae7acb3a990893ab9b3b
Aleksey Redkin <al.red197218@gmail.com>
2025-10-15 23:36:52 +0700

    Создание экшена и редьюсера для панели навигации (навбар)

---
+++ src/services/reducers/navbar.js
@@ -0,0 +1,26 @@
+import {
+  SET_ACTIVE_SECTION_NAVBAR,
+  SET_VISIBILITY_NAVBAR,
+} from "../actions/navbar";
+
+const initialState = {
+  activeSection: "TRIPS",
+  visibility: true,
+};
+
+export const navbarReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_ACTIVE_SECTION_NAVBAR:
+      return {
+        ...state,
+        activeSection: action.activeSection,
+      };
+    case SET_VISIBILITY_NAVBAR:
+      return {
+        ...state,
+        visibility: action.visibility,
+      };
+    default:
+      return state;
+  }
+};
