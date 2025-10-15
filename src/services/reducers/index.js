546aa929425a3cc3d8a31a8108183d63c2188a5a
Aleksey Redkin <al.red197218@gmail.com>
2025-10-16 02:06:23 +0700

    Подключение редьюсера для информации о пользователе

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { navbarReducer } from "./navbar";
 import { profileReducer } from "./profile";
 import { backgroundReducer } from "./background";
+import { userReducer } from "./user";
 
 export const rootReducer = combineReducers({
   navbar: navbarReducer,
   profile: profileReducer,
   background: backgroundReducer,
+  user: userReducer,
 });
