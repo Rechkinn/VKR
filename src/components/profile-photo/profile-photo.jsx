71c9a612546e66c90b0bd121fd22704cbaded490
Aleksey Redkin <al.red197218@gmail.com>
2025-10-15 23:46:03 +0700

    Логика компоннета ProfileInfo переделана с использованием Redux

--- src/components/profile-photo/profile-photo.jsx
+++ src/components/profile-photo/profile-photo.jsx
@@ -1,13 +1,35 @@
 import styles from "./profile-photo.module.css";
 import editProfile from "../../image/edit-profile.svg";
 import Button from "../button/button";
+import { useDispatch } from "react-redux";
+import { SET_ACTIVE_SECTION_PROFILE } from "../../services/actions/profile";
+import { SET_VISIBILITY_NAVBAR } from "../../services/actions/navbar";
+import { SET_SUN_VISIBILITY_ON_BACKGROUND } from "../../services/actions/background";
 
 export default function ProfilePhoto({
   userData,
-  openFormToChangeUserInfo = null,
+  // openFormToChangeUserInfo = null,
+  needButtonToEdit = false,
   size = 166,
   className = "",
 }) {
+  const dispatch = useDispatch();
+
+  function openFormToChangeProfileInfo() {
+    dispatch({
+      type: SET_VISIBILITY_NAVBAR,
+      visibility: false,
+    });
+    dispatch({
+      type: SET_SUN_VISIBILITY_ON_BACKGROUND,
+      sunVisibility: false,
+    });
+    dispatch({
+      type: SET_ACTIVE_SECTION_PROFILE,
+      activeSection: "changeInfo",
+    });
+  }
+
   return (
     <article className={`${className} ${styles.containerPhoto}`}>
       <img
@@ -16,10 +38,10 @@ export default function ProfilePhoto({
         className={styles.photo}
         style={{ width: size, height: size }}
       />
-      {openFormToChangeUserInfo && (
+      {needButtonToEdit && (
         <Button
           className={`onlyIcon ${styles.buttonEdit}`}
-          onClick={openFormToChangeUserInfo}
+          onClick={openFormToChangeProfileInfo}
         >
           <img src={editProfile} alt="Редактировать" />
         </Button>
