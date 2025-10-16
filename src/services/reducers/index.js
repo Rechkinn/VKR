3a6586925bea79438797dd59b2937aaf6bf083fc
Rechkinn <al.red197218@gmail.com>
2025-10-17 00:15:04 +0700

    Подключение редьюсеров tripsReducer и modalReducer в корневой

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -3,10 +3,14 @@ import { navbarReducer } from "./navbar";
 import { profileReducer } from "./profile";
 import { backgroundReducer } from "./background";
 import { userReducer } from "./user";
+import { tripsReducer } from "./trips";
+import { modalReducer } from "./modal";
 
 export const rootReducer = combineReducers({
   navbar: navbarReducer,
   profile: profileReducer,
   background: backgroundReducer,
   user: userReducer,
+  trips: tripsReducer,
+  modal: modalReducer,
 });
