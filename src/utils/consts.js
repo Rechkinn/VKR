9a03a5fa725a7a4173015c7b85afa6dad75be068
Rechkinn <al.red197218@gmail.com>
2025-10-17 00:10:43 +0700

    Добавлены новые константы для вкладок в секции "Поездки"

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,3 +1,7 @@
 export const PROFILE = "PROFILE";
 export const TRIPS = "TRIPS";
 export const CALENDAR = "CALENDAR";
+
+export const ACTIVE_TAB = "Активные";
+export const UPCOMING_TAB = "Предстоящие";
+export const COMPLETED_TAB = "Завершенные";
