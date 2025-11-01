5478ef697777a7af2f09d186aeb7341ebe2218ce
Rechkinn <al.red197218@gmail.com>
2025-11-01 23:20:22 +0700

    Реализация функционала добаления поездки

--- src/components/form-for-new-trip/form-for-new-trip.jsx
+++ src/components/form-for-new-trip/form-for-new-trip.jsx
@@ -9,9 +9,12 @@ import phoneIcon from "../../image/section-trips/phone-icon.svg";
 import Button from "../button/button";
 import { SET_VISIBILITY_NAVBAR } from "../../services/actions/navbar";
 import Input from "../input/input";
+import { useRef } from "react";
 
 export default function FormForNewTrip({ actionType }) {
   const dispatch = useDispatch();
+
+  const formRef = useRef();
 
   function closeForm(event) {
     event.preventDefault();
@@ -24,15 +27,102 @@ export default function FormForNewTrip({ actionType }) {
     });
   }
 
+  function createNewTrip(e) {
+    // departure_datetime
+
+    e.preventDefault();
+
+    console.log("formRef.current");
+    console.log(formRef.current);
+
+    if (!formRef.current) {
+      console.log("formRef для создания заказа не существует");
+      console.log(formRef);
+      return;
+    }
+
+    const newData = {
+      trip_type: "own",
+      is_delegation_active: false,
+    };
+
+    const inputs = formRef.current.elements;
+    console.log("Массив inputs");
+    console.log(inputs);
+
+    let date = "";
+    let time = "";
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].name === "" || inputs[i].value === "") continue;
+
+      if (inputs[i].name === "date") {
+        date = `${inputs[i].value}`;
+        if (date !== "" && time !== "" && !newData?.departure_datetime) {
+          newData["departure_datetime"] = `${date}T${time}:00.007Z`;
+        }
+        continue;
+      }
+      if (inputs[i].name === "time") {
+        time += `${inputs[i].value}`;
+        if (date !== "" && time !== "" && !newData?.departure_datetime) {
+          newData["departure_datetime"] = `${date}T${time}:00.007Z`;
+        }
+        continue;
+      }
+
+      newData[inputs[i].name] = inputs[i].value;
+    }
+
+    console.log("Объект newData");
+    console.log(newData);
+
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzIiwicm9sZSI6ImRyaXZlciIsImV4cCI6MTc2MjA5ODg2NH0.sqHaieRqXjYZCLLfPS_MGcVo1vA4AtrjLR1Pm-20_8A";
+
+    // Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    const option = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(newData),
+    };
+
+    fetch("https://xn--80aqak6ae.xn--p1ai/api/v1/trips", option)
+      .then((response) => {
+        if (!response.ok) throw new Error(`Ошибка ${response.status}`);
+        console.log(response);
+        return response.json();
+      })
+      .then((trip) => {
+        console.log("trip");
+        console.log(trip);
+        closeForm();
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
-    <form className={styles.form}>
+    <form
+      action=""
+      ref={formRef}
+      className={styles.form}
+      onSubmit={(e) => createNewTrip(e)}
+    >
       <Button onClick={closeForm} className={styles.closeButton}>
         <img src={closeIcon} alt="Иконка закрытия формы" />
       </Button>
 
       <div className={styles.containerForTwoInputs}>
         <div className={styles.container40}>
-          <Input label="Дата" iconForLabel={dateIcon} type="date" name="date" />
+          <Input
+            label="Дата"
+            iconForLabel={dateIcon}
+            type="date"
+            name="date"
+            required
+          />
         </div>
         <div className={styles.container60}>
           <Input
@@ -40,6 +130,7 @@ export default function FormForNewTrip({ actionType }) {
             iconForLabel={watchIcon}
             type="time"
             name="time"
+            required
           />
         </div>
       </div>
@@ -48,13 +139,15 @@ export default function FormForNewTrip({ actionType }) {
         label="Откуда"
         iconForLabel={startPointIcon}
         type="text"
-        name="start-point"
+        name="from_address"
+        required
       />
       <Input
         label="Куда"
         iconForLabel={endPointIcon}
         type="text"
-        name="end-point"
+        name="to_address"
+        required
       />
 
       <div className={styles.containerForTwoInputs}>
@@ -63,42 +156,46 @@ export default function FormForNewTrip({ actionType }) {
             label="Пассажиры"
             iconForLabel={dateIcon}
             type="number"
-            name="passengers"
+            name="total_seats"
+            required
           />
         </div>
         <div className={styles.container60}>
-          <label htmlFor="car-class" className={styles.label}>
+          <label htmlFor="car_class" className={styles.label}>
             Класс автомобиля
           </label>
-          <select name="car-class" id="car-class" className={styles.select}>
-            <option value="Эконом">Эконом</option>
-            <option value="Комфорт">Комфорт</option>
-            <option value="Бизнес">Бизнес</option>
+          <select name="car_class" id="car_class" className={styles.select}>
+            <option value="passenger_car">Легковой</option>
+            <option value="minivan">Минивэн</option>
+            <option value="microbus">Микроатобус</option>
+            <option value="bus">Автобус</option>
           </select>
         </div>
       </div>
 
       <Input
-        label="Номер телефона"
+        label="Номер телефона пассажира"
         iconForLabel={phoneIcon}
         type="phone"
-        name="phone"
+        name="passenger_phone_number"
+        required
       />
 
       <div>
-        <label htmlFor="additional-info" className={styles.label}>
+        <label htmlFor="description" className={styles.label}>
           Дополнительная информация
         </label>
         <textarea
-          name="additional-info"
-          id="additional-info"
+          name="description"
+          id="description"
           className={styles.textarea}
         ></textarea>
       </div>
 
       <Button
+        type="submit"
+        // onClick={(e) => e.preventDefault()}
         className={`yellow ${styles.buttonPushForm}`}
-        onClick={(e) => e.preventDefault()}
       >
         Опубликовать
       </Button>
