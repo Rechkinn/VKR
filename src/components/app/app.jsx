8662905217df123751dfa4055d4949ca79099fbe
Rechkinn <al.red197218@gmail.com>
2025-11-04 02:03:11 +0700

    Добавление использования компонента Calendar

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,6 +5,7 @@ import Navbar from "../navbar/navbar";
 import Trips from "../trips/trips";
 import { useSelector } from "react-redux";
 import { CALENDAR, PROFILE, TRIPS } from "../../utils/consts";
+import Calendar from "../calendar/calendar";
 
 export default function App() {
   const { sunVisibility } = useSelector((store) => store.background);
@@ -16,9 +17,7 @@ export default function App() {
       <main>
         {activeSection === PROFILE && <Profile />}
         {activeSection === TRIPS && <Trips />}
-        {activeSection === CALENDAR && (
-          <div>Скоро здесь будет раздел "Календарь"</div>
-        )}
+        {activeSection === CALENDAR && <Calendar />}
         <Navbar />
       </main>
     </>
