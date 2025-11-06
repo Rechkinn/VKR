890592514c460919384baca67ea4077b58efde6c
Aleksey Redkin <al.red197218@gmail.com>
2025-11-06 17:19:55 +0700

    Удален не нужный редьюсер

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import { navbarReducer } from "./navbar";
-import { profileReducer } from "./profile";
 import { backgroundReducer } from "./background";
 import { userReducer } from "./user";
 import { tripsReducer } from "./trips";
@@ -8,7 +7,6 @@ import { modalReducer } from "./modal";
 
 export const rootReducer = combineReducers({
   navbar: navbarReducer,
-  profile: profileReducer,
   background: backgroundReducer,
   user: userReducer,
   trips: tripsReducer,
