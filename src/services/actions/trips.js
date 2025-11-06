df5e0e7f3d981bbbcc866d68b93e4d435a8471ac
Rechkinn <al.red197218@gmail.com>
2025-11-06 20:24:57 +0700

    Добавлено выполнение запроса на сервер

--- src/services/actions/trips.js
+++ src/services/actions/trips.js
@@ -1,4 +1,8 @@
+import { doRequest } from "../../utils/doRequest";
+
 export const SET_CURRENT_TAB = "SET_CURRENT_TAB";
+export const ADD_TRIP = "ADD_TRIP";
+
 export const GET_TRIPS_REQUEST = "GET_TRIPS_REQUEST";
 export const GET_TRIPS_REQUEST_ERROR = "GET_TRIPS_REQUEST_ERROR";
 export const GET_TRIPS_REQUEST_SUCCESS = "GET_TRIPS_REQUEST_SUCCESS";
@@ -10,12 +14,24 @@ export function getTrips() {
     });
 
     const option = {
-      method: "POST",
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("access_token")}`,
       },
-      body: JSON.stringify(newData),
     };
+
+    doRequest("/trips/search?trip_type=delegated&skip=0&limit=50", option)
+      .then((trips) => {
+        dispatch({
+          type: GET_TRIPS_REQUEST_SUCCESS,
+          trips: trips,
+        });
+      })
+      .catch(
+        dispatch({
+          type: GET_TRIPS_REQUEST_ERROR,
+        })
+      );
   };
 }
