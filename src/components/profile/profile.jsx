bb7958d2ae2c2f7b69aa68b8711fd136e1dd1ac6
Aleksey Redkin <al.red197218@gmail.com>
2025-11-06 17:17:09 +0700

    Компонент переделан под компонент-обёртку

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,41 +1,12 @@
-import Cars from "../cars/cars";
-import ProfileInfo from "../profile-info/profile-info";
-import styles from "./profile.module.css";
-import ChangeProfileInfo from "../change-profile-info/change-profile-info";
-import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export default function Profile() {
-  const { activeSection } = useSelector((store) => store.profile);
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  // if (!userInfo) return <div style={{ color: "#666" }}>No user info</div>;
-  // let parsed;
-  // try {
-  //   parsed = typeof userInfo === "string" ? JSON.parse(userInfo) : userInfo;
-  // } catch (err) {
-  //   return (
-  //     <div style={{ color: "#d32f2f" }}>
-  //       <div>Invalid JSON: {err.message}</div>
-  //       <pre style={{ whiteSpace: "pre-wrap" }}>{String(userInfo)}</pre>
-  //     </div>
-  //   );
-  // }
-  // if (!parsed || typeof parsed !== "object") {
-  //   return <div>{String(parsed)}</div>;
-  // }
-  // setUserData(parsed);
-  // }, []);
+  useEffect(() => {
+    navigate("/profile");
+  }, []);
 
-  return (
-    <>
-      {activeSection === "info" && (
-        <>
-          <ProfileInfo />
-          {/* Здесь будет компонент для отображения автомобилей */}
-          {/* <Cars /> */}
-        </>
-      )}
-      {activeSection === "changeInfo" && <ChangeProfileInfo />}
-    </>
-  );
+  return <Outlet />;
 }
