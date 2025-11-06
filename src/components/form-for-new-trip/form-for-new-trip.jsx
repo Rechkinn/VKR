63005ee0dc314a8f46ace83501cee8f859fa4ac7
Aleksey Redkin <al.red197218@gmail.com>
2025-11-06 19:54:28 +0700

    Изменение компонента формы

--- src/components/form-for-new-trip/form-for-new-trip.jsx
+++ src/components/form-for-new-trip/form-for-new-trip.jsx
@@ -9,30 +9,30 @@ import phoneIcon from "../../image/section-trips/phone-icon.svg";
 import Button from "../button/button";
 import { SET_VISIBILITY_NAVBAR } from "../../services/actions/navbar";
 import Input from "../input/input";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router";
 
-export default function FormForNewTrip({
-  actionType,
-  rerender,
-  functionForRerender,
-}) {
+export default function FormForNewTrip({ functionForRerender, toRoute }) {
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   const formRef = useRef();
 
-  function closeForm() {
+  useEffect(() => {
     dispatch({
-      type: actionType,
+      type: SET_VISIBILITY_NAVBAR,
+      visibility: false,
     });
+  }, []);
+
+  function closeForm() {
     dispatch({
       type: SET_VISIBILITY_NAVBAR,
       visibility: true,
     });
+    navigate(toRoute);
   }
 
   function createNewTrip(e) {
-    // departure_datetime
-
     e.preventDefault();
 
     console.log("formRef.current");
@@ -88,10 +88,6 @@ export default function FormForNewTrip({
     console.log("Объект newData");
     console.log(newData);
 
-    // const token =
-    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzIiwicm9sZSI6ImRyaXZlciIsImV4cCI6MTc2MjA5ODg2NH0.sqHaieRqXjYZCLLfPS_MGcVo1vA4AtrjLR1Pm-20_8A";
-    // Authorization: `Bearer ${token}`,
-
     const option = {
       method: "POST",
       headers: {
@@ -117,117 +113,121 @@ export default function FormForNewTrip({
   }
 
   return (
-    <form
-      action=""
-      ref={formRef}
-      className={styles.form}
-      onSubmit={(e) => createNewTrip(e)}
-    >
-      <Button
-        onClick={(e) => {
-          e.preventDefault();
-          closeForm();
-        }}
-        className={styles.closeButton}
-      >
-        <img src={closeIcon} alt="Иконка закрытия формы" />
-      </Button>
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Новая поездка</h1>
+      </header>
 
-      <div className={styles.containerForTwoInputs}>
-        <div className={styles.container40}>
+      <div className={styles.containerCreateTrip}>
+        <form
+          action=""
+          ref={formRef}
+          className={styles.form}
+          onSubmit={(e) => createNewTrip(e)}
+        >
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              closeForm();
+            }}
+            className={styles.closeButton}
+          >
+            <img src={closeIcon} alt="Иконка закрытия формы" />
+          </Button>
+
+          <div className={styles.containerForTwoInputs}>
+            <div className={styles.container40}>
+              <Input
+                label="Дата"
+                iconForLabel={dateIcon}
+                type="date"
+                name="date"
+                required
+              />
+            </div>
+            <div className={styles.container60}>
+              <Input
+                label="Время"
+                iconForLabel={watchIcon}
+                type="time"
+                name="time"
+                required
+              />
+            </div>
+          </div>
+
           <Input
-            label="Дата"
-            iconForLabel={dateIcon}
-            type="date"
-            name="date"
+            label="Откуда"
+            iconForLabel={startPointIcon}
+            type="text"
+            name="from_address"
             required
           />
-        </div>
-        <div className={styles.container60}>
+
           <Input
-            label="Время"
-            iconForLabel={watchIcon}
-            type="time"
-            name="time"
+            label="Куда"
+            iconForLabel={endPointIcon}
+            type="text"
+            name="to_address"
             required
           />
-        </div>
-      </div>
 
-      <Input
-        label="Откуда"
-        iconForLabel={startPointIcon}
-        type="text"
-        name="from_address"
-        required
-      />
+          <div className={styles.containerForTwoInputs}>
+            <div className={styles.container40}>
+              <Input
+                label="Пассажиры"
+                iconForLabel={dateIcon}
+                type="number"
+                name="total_seats"
+                required
+              />
+            </div>
+            <div className={styles.container60}>
+              <label htmlFor="car_class" className={styles.label}>
+                Класс автомобиля
+              </label>
+              <select name="car_class" id="car_class" className={styles.select}>
+                <option value="passenger_car">Легковой</option>
+                <option value="minivan">Минивэн</option>
+                <option value="microbus">Микроатобус</option>
+                <option value="bus">Автобус</option>
+              </select>
+            </div>
+          </div>
 
-      <Input
-        label="Куда"
-        iconForLabel={endPointIcon}
-        type="text"
-        name="to_address"
-        required
-      />
-
-      <div className={styles.containerForTwoInputs}>
-        <div className={styles.container40}>
           <Input
-            label="Пассажиры"
-            iconForLabel={dateIcon}
+            label="Номер телефона пассажира"
+            iconForLabel={phoneIcon}
+            type="tel"
+            name="passenger_phone_number"
+            required
+          />
+
+          <Input label="Стоимость" type="number" name="price" />
+
+          <Input
+            label="Комссия"
             type="number"
-            name="total_seats"
+            name="delegation_commission"
             required
           />
-        </div>
-        <div className={styles.container60}>
-          <label htmlFor="car_class" className={styles.label}>
-            Класс автомобиля
-          </label>
-          <select name="car_class" id="car_class" className={styles.select}>
-            <option value="passenger_car">Легковой</option>
-            <option value="minivan">Минивэн</option>
-            <option value="microbus">Микроатобус</option>
-            <option value="bus">Автобус</option>
-          </select>
-        </div>
+
+          <div>
+            <label htmlFor="description" className={styles.label}>
+              Дополнительная информация
+            </label>
+            <textarea
+              name="description"
+              id="description"
+              className={styles.textarea}
+            ></textarea>
+          </div>
+
+          <Button type="submit" className={`yellow ${styles.buttonPushForm}`}>
+            Отправить в канал
+          </Button>
+        </form>
       </div>
-
-      <Input
-        label="Номер телефона пассажира"
-        iconForLabel={phoneIcon}
-        type="tel"
-        name="passenger_phone_number"
-        required
-      />
-
-      <Input label="Стоимость" type="number" name="price" />
-
-      <Input
-        label="Комссия"
-        type="number"
-        name="delegation_commission"
-        required
-      />
-
-      <div>
-        <label htmlFor="description" className={styles.label}>
-          Дополнительная информация
-        </label>
-        <textarea
-          name="description"
-          id="description"
-          className={styles.textarea}
-        ></textarea>
-      </div>
-
-      <Button
-        type="submit"
-        // onClick={(e) => e.preventDefault()}
-        className={`yellow ${styles.buttonPushForm}`}
-      >
-        Отправить в канал
-      </Button>
-    </form>
+    </>
   );
 }
