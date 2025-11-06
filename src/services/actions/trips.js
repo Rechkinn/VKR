d31fea5aadec555d0e7f8a3f3148988e8c6d86fe
Aleksey Redkin <al.red197218@gmail.com>
2025-11-06 19:55:42 +0700

    Добавлены новые экшены (не полностью)

--- src/services/actions/trips.js
+++ src/services/actions/trips.js
@@ -1,3 +1,21 @@
 export const SET_CURRENT_TAB = "SET_CURRENT_TAB";
-export const OPEN_FORM_SECTION_TRIP = "OPEN_FORM";
-export const CLOSE_FORM_SECTION_TRIP = "CLOSE_FORM";
+export const GET_TRIPS_REQUEST = "GET_TRIPS_REQUEST";
+export const GET_TRIPS_REQUEST_ERROR = "GET_TRIPS_REQUEST_ERROR";
+export const GET_TRIPS_REQUEST_SUCCESS = "GET_TRIPS_REQUEST_SUCCESS";
+
+export function getTrips() {
+  return function (dispatch) {
+    dispatch({
+      type: GET_TRIPS_REQUEST,
+    });
+
+    const option = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+      body: JSON.stringify(newData),
+    };
+  };
+}
