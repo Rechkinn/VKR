ecd30bbfbee16280c367f071cfc71dcc5d63d11e
Rechkinn <al.red197218@gmail.com>
2025-11-06 22:14:28 +0700

    Добавлено определение маршрута в панели навигации для автоматического регулирования скрытия и показа навбара

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,16 +6,31 @@ import userActiveIcon from "../../image/navbar/userActive.svg";
 import carActiveIcon from "../../image/navbar/carActive.svg";
 import calendarActiveIcon from "../../image/navbar/calendarActive.svg";
 import { useDispatch, useSelector } from "react-redux";
-import { SET_ACTIVE_SECTION_NAVBAR } from "../../services/actions/navbar";
+import {
+  SET_ACTIVE_SECTION_NAVBAR,
+  SET_VISIBILITY_NAVBAR,
+} from "../../services/actions/navbar";
 import { CALENDAR, PROFILE, TRIPS } from "../../utils/consts";
 import NavbarLink from "../navbar-link/navbar-link";
+import { useLocation } from "react-router";
+import { useEffect } from "react";
 
 export default function Navbar() {
   const { visibility, activeSection } = useSelector((store) => store.navbar);
-
-  console.log("activeSection", activeSection);
-
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const visibilityNavbar =
+      location.pathname === "/profile/change" ||
+      location.pathname === "/create-new-trip";
+
+    dispatch({
+      type: SET_VISIBILITY_NAVBAR,
+      visibility: !visibilityNavbar,
+    });
+  }, [pathname]);
+
   function setActiveSection(section) {
     dispatch({
       type: SET_ACTIVE_SECTION_NAVBAR,
