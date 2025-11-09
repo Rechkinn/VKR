3d47e35700bd15be9f6db76d07faaa80385db64c
Aleksey Redkin <al.red197218@gmail.com>
2025-11-09 17:28:32 +0700

    Добавление логирования

--- src/components/trips/trips.jsx
+++ src/components/trips/trips.jsx
@@ -22,6 +22,9 @@ export default function Trips() {
     (store) => store.trips
   );
 
+  console.log("trips");
+  console.log(trips);
+
   const { visibilityModal, openModal, closeModal } = useModal();
 
   const [styleTripsContainer, setStyleTripsContainer] = useState(null);
