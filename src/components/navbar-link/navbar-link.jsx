72138e30548e28bbf2fb0f3a2d9048f503bbe5ab
Rechkinn <al.red197218@gmail.com>
2025-11-10 23:23:09 +0700

    Изменение структуры компонента и получаемых пропсов

--- src/components/navbar-link/navbar-link.jsx
+++ src/components/navbar-link/navbar-link.jsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
 import styles from "./navbar-link.module.css";
-import { NavLink } from "react-router";
+import { Link, NavLink, useLocation } from "react-router";
 import { useDispatch } from "react-redux";
 import { SET_ACTIVE_SECTION_NAVBAR } from "../../services/actions/navbar";
 
-export default function NavbarLink({ children, toRoute, icon }) {
+export default function NavbarLink({ children, toRoute, activeIcon, icon }) {
+  const { pathname } = useLocation();
+  console.log(pathname);
+
   return (
-    <NavLink
+    <Link
       to={toRoute}
-      className={({ isActive }) => {
-        return isActive ? styles.activeLink : styles.link;
-      }}
+      className={pathname === toRoute ? styles.activeLink : styles.link}
     >
-      <img src={icon} alt="" className={styles.icon} />
+      <img
+        src={pathname === toRoute ? activeIcon : icon}
+        alt=""
+        className={styles.icon}
+      />
       <p>{children}</p>
-    </NavLink>
+    </Link>
   );
 }
