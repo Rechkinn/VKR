b2cbd5b39663f93ac159073b70fbd0f9716a4429
Rechkinn <al.red197218@gmail.com>
2025-11-19 16:15:15 +0700

    Использован новый компонент CarImage

--- src/components/car/car.jsx
+++ src/components/car/car.jsx
@@ -1,12 +1,12 @@
 import Button from "../button/button";
 import styles from "./car.module.css";
 import settingsIcon from "../../image/section-trips/settings-icon.svg";
-import exampleCar from "../../image/camryPhoto.png";
+import CarImage from "../car-image/car-image";
 
 export default function Car({ trip }) {
   return (
     <article className={styles.car}>
-      <img src={exampleCar} alt="" className={styles.img} />
+      <CarImage />
 
       <div>
         <h3 className={styles.name}>Toyota Camry</h3>
