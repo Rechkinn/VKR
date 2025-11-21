c503e044d8affae72d312f0416abd802a648ef85
Rechkinn <al.red197218@gmail.com>
2025-11-21 23:14:46 +0700

    Добавление логики получения списка автомобилей

--- src/services/actions/car.js
+++ src/services/actions/car.js
@@ -4,6 +4,39 @@ export const CAR_CREATE_REQUEST = "CAR_CREATE_REQUEST";
 export const CAR_CREATE_REQUEST_ERROR = "CAR_CREATE_REQUEST_ERROR";
 export const CAR_CREATE_REQUEST_SUCCESS = "CAR_CREATE_REQUEST_SUCCESS";
 
+export const GET_CARS_REQUEST = "GET_CARS_REQUEST";
+export const GET_CARS_REQUEST_ERROR = "GET_CARS_REQUEST_ERROR";
+export const GET_CARS_REQUEST_SUCCESS = "GET_CARS_REQUEST_SUCCESS";
+
+export function getCars() {
+  return function (dispatch) {
+    dispatch({
+      type: GET_CARS_REQUEST,
+    });
+
+    const option = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+    };
+
+    doRequest("/vehicles", option)
+      .then((cars) => {
+        dispatch({
+          type: GET_CARS_REQUEST_SUCCESS,
+          cars: cars,
+        });
+      })
+      .catch(() => {
+        dispatch({
+          type: GET_CARS_REQUEST_ERROR,
+        });
+      });
+  };
+}
+
 export function createCar(car) {
   return function (dispatch) {
     dispatch({
--- src/services/reducers/car.js
+++ src/services/reducers/car.js
@@ -2,12 +2,18 @@ import {
   CAR_CREATE_REQUEST,
   CAR_CREATE_REQUEST_ERROR,
   CAR_CREATE_REQUEST_SUCCESS,
+  GET_CARS_REQUEST,
+  GET_CARS_REQUEST_ERROR,
+  GET_CARS_REQUEST_SUCCESS,
 } from "../actions/car";
 
 const initialState = {
   cars: [],
   createCarRequest: false,
   createCarRequestError: false,
+
+  getCarsRequest: false,
+  getCarsRequestError: false,
 };
 
 export const carReducer = (state = initialState, action) => {
@@ -30,6 +36,25 @@ export const carReducer = (state = initialState, action) => {
         cars: [...cars, action.newCar],
         createCarRequest: false,
         createCarRequestError: false,
+      };
+    case GET_CARS_REQUEST:
+      return {
+        ...state,
+        getCarsRequest: true,
+        getCarsRequestError: false,
+      };
+    case GET_CARS_REQUEST_ERROR:
+      return {
+        ...state,
+        getCarsRequest: false,
+        getCarsRequestError: true,
+      };
+    case GET_CARS_REQUEST_SUCCESS:
+      return {
+        ...state,
+        cars: [...action.cars],
+        getCarsRequest: false,
+        getCarsRequestError: false,
       };
     default:
       return state;
