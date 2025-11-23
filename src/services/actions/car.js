43743c13dbe0aba28e283d8177fef33bf7ffa41d
Rechkinn <al.red197218@gmail.com>
2025-11-23 16:18:48 +0700

    Подготовка логики для изменения данных об автомобиле

--- src/services/actions/car.js
+++ src/services/actions/car.js
@@ -8,6 +8,12 @@ export const GET_CARS_REQUEST = "GET_CARS_REQUEST";
 export const GET_CARS_REQUEST_ERROR = "GET_CARS_REQUEST_ERROR";
 export const GET_CARS_REQUEST_SUCCESS = "GET_CARS_REQUEST_SUCCESS";
 
+export const SET_CAR_FOR_SETTINGS = "SET_CAR_FOR_SETTINGS";
+
+export const EDIT_CAR_REQUEST = "EDIT_CAR_REQUEST";
+export const EDIT_CAR_REQUEST_ERROR = "EDIT_CAR_REQUEST_ERROR";
+export const EDIT_CAR_REQUEST_SUCCESS = "EDIT_CAR_REQUEST_SUCCESS";
+
 export function getCars() {
   return function (dispatch) {
     dispatch({
@@ -65,6 +71,39 @@ export function createCar(car, closeCarForm = () => {}) {
       .catch(() => {
         dispatch({
           type: CAR_CREATE_REQUEST_ERROR,
+        });
+      });
+  };
+}
+
+export function editCar(carId, car, closeCarForm = () => {}) {
+  return function (dispatch) {
+    dispatch({
+      type: EDIT_CAR_REQUEST,
+    });
+
+    const option = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+      body: JSON.stringify(car),
+    };
+
+    doRequest(`/vehicles/${carId}`, option)
+      .then((updatingCar) => {
+        console.log("updatingCar с сервера пришло");
+        console.log(updatingCar);
+        dispatch({
+          type: EDIT_CAR_REQUEST_SUCCESS,
+          updatingCar: updatingCar,
+        });
+        closeCarForm();
+      })
+      .catch(() => {
+        dispatch({
+          type: EDIT_CAR_REQUEST_ERROR,
         });
       });
   };
--- src/services/reducers/car.js
+++ src/services/reducers/car.js
@@ -2,22 +2,105 @@ import {
   CAR_CREATE_REQUEST,
   CAR_CREATE_REQUEST_ERROR,
   CAR_CREATE_REQUEST_SUCCESS,
+  EDIT_CAR_REQUEST,
+  EDIT_CAR_REQUEST_ERROR,
+  EDIT_CAR_REQUEST_SUCCESS,
   GET_CARS_REQUEST,
   GET_CARS_REQUEST_ERROR,
   GET_CARS_REQUEST_SUCCESS,
+  SET_CAR_FOR_SETTINGS,
 } from "../actions/car";
 
 const initialState = {
+  // cars: [
+  //   {
+  //     brand: "Mercedes-Benz",
+  //     model: "AMG GTS 500",
+  //     year: 0,
+  //     color: "string",
+  //     license_plate: "А123МР77",
+  //     additional_info: "qwe wq eqwe qwe wqe qwe qwe qwe qwee",
+  //     car_class: "microbus",
+  //     id: 1,
+  //     driver_id: 0,
+  //     photo_url: "string",
+  //     is_active: false,
+  //   },
+  //   {
+  //     brand: "string",
+  //     model: "string",
+  //     year: 0,
+  //     color: "string",
+  //     license_plate: "string",
+  //     additional_info: "QWEWQE WEQWE qwe",
+  //     car_class: "microbus",
+  //     id: 2,
+  //     driver_id: 0,
+  //     photo_url: "string",
+  //     is_active: false,
+  //   },
+  //   {
+  //     brand: "string",
+  //     model: "string",
+  //     year: 0,
+  //     color: "string",
+  //     license_plate: "string",
+  //     additional_info: "123 123 123 123 12 3123 222",
+  //     car_class: "bus",
+  //     id: 3,
+  //     driver_id: 0,
+  //     photo_url: "string",
+  //     is_active: true,
+  //   },
+  // ],
   cars: [],
   createCarRequest: false,
   createCarRequestError: false,
 
   getCarsRequest: false,
   getCarsRequestError: false,
+
+  carForSettings: null,
+
+  editCarRequest: false,
+  editCarRequestError: false,
 };
 
 export const carReducer = (state = initialState, action) => {
   switch (action.type) {
+    case EDIT_CAR_REQUEST:
+      return {
+        ...state,
+        editCarRequest: true,
+        editCarRequestError: false,
+      };
+    case EDIT_CAR_REQUEST_ERROR:
+      return {
+        ...state,
+        editCarRequest: false,
+        editCarRequestError: true,
+      };
+    case EDIT_CAR_REQUEST_SUCCESS:
+      return {
+        ...state,
+        cars: [
+          ...state.cars.filter((car) => {
+            if (car.id === action.updatingCar.id) {
+              return action.updatingCar;
+            } else {
+              return car;
+            }
+          }),
+        ],
+        editCarRequest: false,
+        editCarRequestError: false,
+      };
+
+    case SET_CAR_FOR_SETTINGS:
+      return {
+        ...state,
+        carForSettings: action.carForSettings,
+      };
     case CAR_CREATE_REQUEST:
       return {
         ...state,
