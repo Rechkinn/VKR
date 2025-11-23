74ca64acab1be7a2cd431da84d324f865a199261
Rechkinn <al.red197218@gmail.com>
2025-11-23 16:23:22 +0700

    Добавлен новый пропс

--- src/components/car/car.jsx
+++ src/components/car/car.jsx
@@ -2,8 +2,9 @@ import Button from "../button/button";
 import styles from "./car.module.css";
 import settingsIcon from "../../image/section-trips/settings-icon.svg";
 import CarImage from "../car-image/car-image";
+import Status from "../status/status";
 
-export default function Car({ car }) {
+export default function Car({ car, openSettings }) {
   return (
     <article className={styles.car}>
       <CarImage />
@@ -14,9 +15,9 @@ export default function Car({ car }) {
         <p className={styles.registrationNumber}>{car.license_plate}</p>
       </div>
 
-      {car.is_active && <div className={styles.carStatus}>Активно</div>}
+      {car.is_active && <Status extraClass={styles.carStatus}>Активно</Status>}
 
-      <Button className={styles.settingsButton}>
+      <Button onClick={openSettings} className={styles.settingsButton}>
         <img src={settingsIcon} alt="" />
       </Button>
     </article>
