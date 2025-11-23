5937a88c7f4a30b8664237c461d571bd119a8231
Rechkinn <al.red197218@gmail.com>
2025-11-23 16:27:56 +0700

    Добавлены новые пропсы

--- src/components/select-custom/select-custom.jsx
+++ src/components/select-custom/select-custom.jsx
@@ -1,15 +1,21 @@
 import styles from "./select-custom.module.css";
 
-const SelectCustom = ({ label }) => {
+const SelectCustom = ({ label, disabled, defaultValue }) => {
   return (
     <div className={styles.container}>
       <label htmlFor="car_class" className={styles.label}>
         {label}
       </label>
-      <select name="car_class" id="car_class" className={styles.select}>
+      <select
+        name="car_class"
+        id="car_class"
+        className={styles.select}
+        disabled={disabled}
+        defaultValue={defaultValue}
+      >
         <option value="passenger_car">Легковой</option>
         <option value="minivan">Минивэн</option>
-        <option value="microbus">Микроатобус</option>
+        <option value="microbus">Микроавтобус</option>
         <option value="bus">Автобус</option>
       </select>
     </div>
