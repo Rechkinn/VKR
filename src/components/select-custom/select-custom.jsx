a7a7dc53657cccfecf4926a00da5a6aa184178a0
Rechkinn <al.red197218@gmail.com>
2025-12-09 18:14:47 +0700

    Исправлен баг по поводу не правильного отображения значения по умолчанию в селекте и исправлен баг по поводу невозможности изменения значения селекта

--- src/components/select-custom/select-custom.jsx
+++ src/components/select-custom/select-custom.jsx
@@ -1,17 +1,25 @@
+import { useEffect, useState } from "react";
 import styles from "./select-custom.module.css";
 
-const SelectCustom = ({ label, disabled, defaultValue }) => {
+const SelectCustom = ({ label, disabled = false, defaultValue }) => {
+  const [option, setOption] = useState();
+  console.log("option", option);
+
+  useEffect(() => {
+    setOption(defaultValue);
+  }, [defaultValue]);
+
   return (
     <div className={styles.container}>
       <label htmlFor="car_class" className={styles.label}>
         {label}
       </label>
       <select
+        value={option}
+        onChange={(e) => setOption(e.target.value)}
         name="car_class"
         id="car_class"
         className={styles.select}
-        disabled={disabled}
-        value={defaultValue}
       >
         <option value="passenger_car">Легковой</option>
         <option value="minivan">Минивэн</option>
