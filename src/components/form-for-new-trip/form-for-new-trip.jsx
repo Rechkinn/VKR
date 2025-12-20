a0ef7c3524727ddfd49135ce1814e48b5b998c9e
Rechkinn <al.red197218@gmail.com>
2025-12-21 00:39:56 +0700

    Изменение текста ошибки

--- src/components/form-for-new-trip/form-for-new-trip.jsx
+++ src/components/form-for-new-trip/form-for-new-trip.jsx
@@ -304,7 +304,7 @@ export default function FormForNewTrip() {
                 className={styles.inputDate}
                 errorText={
                   dateError
-                    ? "Введите дату больше от сегодняшнего дня, но не более чем на год вперёд"
+                    ? "Введите дату от сегодняшнего дня, но не более чем на год вперёд"
                     : ""
                 }
                 required
